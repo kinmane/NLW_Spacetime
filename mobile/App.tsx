d77d24a3f344eae6369398794aed1f5d5b9b47c0
Gabriel Souza <rorato91@gmail.com>
2023-05-18 09:00:53 -0300

    dia 2 mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-zinc-50">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="light" />
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-title text-5xl text-gray-50">Rocketseat</Text>
+      <StatusBar style="light" translucent />
     </View>
   )
 }
